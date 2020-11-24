5cc02d5032ff5774517403495adc6abc02343917
norman-schmidt <60552466+norman-schmidt@users.noreply.github.com>
2020-11-24 22:13:17 +0100

    installed material ui + implemented Header.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import React from 'react'
 
+import Header from './components/Header'
+
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
+import Container from '@material-ui/core/Container'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#673ab7'
+    },
+    secondary: {
+      main: '#f50057'
+    },
+    tonalOffset: 0.2
+  }
+})
+
+// const useStyles = makeStyles({
+//   root: {
+//     background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+//     maxWidth: 960
+//   }
+// })
+
 function App () {
+  // const classes = useStyles()
   return (
-    <div className="App">
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container maxWidth="md">
+        <Header></Header>
+      </Container>
+    </ThemeProvider>
   )
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,100 @@
+import React from 'react'
+
+import { makeStyles, fade } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import IconButton from '@material-ui/core/IconButton'
+import Typography from '@material-ui/core/Typography'
+import InputBase from '@material-ui/core/InputBase'
+import LaptopChromebook from '@material-ui/icons/LaptopChromebook'
+import SearchIcon from '@material-ui/icons/Search'
+
+const useStyles = makeStyles((theme) => ({
+  rentitButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block'
+    },
+    fontFamily: 'Consolas'
+  },
+  search: {
+    flexGrow: 1,
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25)
+    },
+    marginLeft: 0,
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(3)
+    },
+    width: 'auto'
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  inputRoot: {
+    color: 'inherit',
+    width: '100%'
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`
+    // [theme.breakpoints.up('sm')]: {
+    //   width: '12ch',
+    // }
+    // transition
+  }
+}))
+
+function Header () {
+  const classes = useStyles()
+
+  return (
+    <div>
+      <AppBar position="sticky">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.rentitButton}
+            color="inherit"
+            aria-label="rentit-button"
+          >
+            <LaptopChromebook />
+          </IconButton>
+          <Typography className={classes.title} variant="h4" noWrap>
+            RENTIT
+          </Typography>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <div>
+                <InputBase
+                placeholder="Search…"
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput
+                }}
+                inputProps={{ 'aria-label': 'search' }}
+                />
+            </div>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
+  )
+}
+
+export default Header
