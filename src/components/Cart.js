c047cb9dd89faf4465df706cc60d7cf9cc217aaf
norman-schmidt <60552466+norman-schmidt@users.noreply.github.com>
2021-01-22 01:47:03 +0100

    checkoutButton

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { Typography } from '@material-ui/core'
+import { Typography, Button, makeStyles, Grid } from '@material-ui/core'
 import ArticleListItem from './Articles/ArticleListItem'
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos'
+
+const useStyles = makeStyles((theme) => ({
+  checkoutButton: {
+    display: 'flex',
+    margin: '20px auto',
+    height: 50
+  }
+}))
 
 const Cart = () => {
+  const classes = useStyles()
+
   const cart = useSelector(state => state.cart)
 
   console.log(cart)
@@ -11,9 +22,22 @@ const Cart = () => {
   return (
         <div>
             {cart && cart.items.length > 0
+              ? <Grid container justify="center" alignItems="center">
+                  <Grid item xs={12} md={6} align="center">
+                    <Typography variant='h5'>Total: </Typography>
+                  </Grid>
+                  <Grid item xs={12} md={6} justify="center">
+                    <Button className={classes.checkoutButton} variant="contained" color="primary"endIcon={<ArrowForwardIosIcon />}>Proceed to Checkout</Button>
+                  </Grid>
+                </Grid>
+              : <div></div>
+            }
+            {cart && cart.items.length > 0
               ? cart.items.map((item, index) => {
                   return (
-                        <ArticleListItem key={index} articleId={item.articleId}></ArticleListItem>
+                    <div key={index}>
+                      <ArticleListItem articleId={item.articleId}></ArticleListItem>
+                    </div>
                   )
                 })
               : <Typography align='center'>Your cart is empty.</Typography>
