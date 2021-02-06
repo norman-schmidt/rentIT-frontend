c35954aa4e2309272ae50ebc5a7525fabe2ef502
norman-schmidt <60552466+norman-schmidt@users.noreply.github.com>
2021-02-06 01:58:01 +0100

    Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,29 @@
 /* eslint-disable react/prop-types */
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import Card from '@material-ui/core/Card'
 import CardActionArea from '@material-ui/core/CardActionArea'
 import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
 import Typography from '@material-ui/core/Typography'
-
-import Axios from 'axios'
-import { API_ENDPOINT } from '../config'
+import ImageGallery from 'react-image-gallery'
+import './Home.css'
+import { Button, Container, Grid } from '@material-ui/core'
+import { useSelector } from 'react-redux'
 
 const useStyles = makeStyles({
   root: {
-    paddingTop: 16
+    paddingTop: 30
+  },
+  login: {
+    margin: '40px 0'
+  },
+  images: {
+    marginTop: 50
   },
   card: {
     width: '80%',
-    marginTop: 30
+    marginTop: 70
   },
   media: {
     height: 200
@@ -25,41 +32,79 @@ const useStyles = makeStyles({
 
 function Home (props) {
   const classes = useStyles()
-  const [categories, setCategories] = useState([])
-
-  useEffect(() => {
-    Axios.get(API_ENDPOINT + 'categories/name/')
-      .then(res => {
-        setCategories(res.data)
-      })
-  }, [])
+  const { isLoggedIn } = useSelector(state => state.auth)
+  const images = [
+    {
+      original: 'https://image.coolblue.de/840x473/content/8dc89da899918592b732df02b014d56d'
+    },
+    {
+      original: 'https://static.bhphotovideo.com/explora/sites/default/files/ts-big-tv-big-game.jpg'
+    },
+    {
+      original: 'https://bilder.t-online.de/b/86/17/87/66/id_86178766/tid_da/mehr-ueberblick-im-homeoffice-wir-klaeren-welche-pc-monitore-empfehlenswert-sind-.jpg'
+    },
+    {
+      original: 'https://cdn.mos.cms.futurecdn.net/h65MBiLdc2Et2YVXuJ8kCR.jpg'
+    }
+  ]
 
   return (
-    <div className={classes.root} align="center">
+    <Container className={classes.root} align="center">
       <Typography variant="h4" align="center" paragraph>Willkommen bei RentIT</Typography>
       <Typography variant="subtitle1" align="center" paragraph>Ihr Ort um IT-Geräte bequem auszuleihen.</Typography>
-
-      {categories.map((categorie, i) => {
-        return (
-          <Card key={i} className={classes.card}>
-            <CardActionArea onClick={() => props.history.push('/categories/' + categorie)}>
-              <CardMedia
-                className={classes.media}
-                image="https://mediacloud.kiplinger.com/image/private/s--r6zIX9Es--/v1602874257/Investing/small-cap-tech-stocks.jpg"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h6" component="h6">
-                  {categorie.toUpperCase()}
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-
-              </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        )
-      })}
-    </div>
+      {!isLoggedIn
+        ? <div>
+          <Grid className={classes.login} container justify='space-around'>
+            <Grid item xs={12} sm={4}>
+              <Button fullWidth variant="outlined" color='primary' onClick={() => props.history.push('/login')}>
+                Login
+              </Button>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Button fullWidth variant="contained" color='primary' onClick={() => props.history.push('/register')}>
+                Register
+              </Button>
+            </Grid>
+          </Grid>
+            </div>
+        : <div></div>
+      }
+      <div className={classes.images}>
+        <ImageGallery items={images} autoPlay={true} showThumbnails={false} showFullscreenButton={false} showPlayButton={false}/>
+      </div>
+      <Card elevation={2} className={classes.card}>
+        <CardActionArea onClick={() => props.history.push('/categories')}>
+          <CardMedia
+            className={classes.media}
+            image="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAsTWIn.img?h=416&w=799&m=6&q=60&u=t&o=f&l=f&x=675&y=408"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h6" component="h6">
+              Check our categories 💻🖥🖨📱
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              ... and find something you like!
+          </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+      <Card elevation={2} className={classes.card}>
+        <CardActionArea onClick={() => props.history.push('/cart')}>
+          <CardMedia
+            className={classes.media}
+            image="https://cdn1.static-tgdp.com/ui/productimages/approved/std.lang.all/95/32/689532_sized_1800x1200_rev_2.jpg"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h6" component="h6">
+              Already put something in the cart? 📝
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              Time to rent!
+          </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </Container>
   )
 }
 
